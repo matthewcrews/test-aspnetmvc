607a578730374c149e84e1da2c02145bdc025e61
mcrews <matthewcrews@gmail.com>
2018-01-30 07:41:49 -0800

    Initial Commit

---
+++ TestWebsite/IGreeter.cs
@@ -0,0 +1,15 @@
+﻿namespace TestWebsite
+{
+    public interface IGreeter
+    {
+        string GetMessageOfDay();
+    }
+
+    public class Greeter : IGreeter
+    {
+        public string GetMessageOfDay()
+        {
+            return "Howdy Do!";
+        }
+    }
+}
